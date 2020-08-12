6e71877bd06258609f3a43f1da7d56da817a5e3f
Lucas Nogueira <lucas.nogueira@softwire.com>
2020-08-12 09:29:49 +0100

    M3: split items into todo, doing and done lists.

--- test_indexViewModel.py
+++ test_indexViewModel.py
@@ -5,23 +5,55 @@ from to_do_item import Status
 
 
 def test_items():
+    # given
     item1 = ToDoItem(1, "First task", Status.DONE)
     item2 = ToDoItem(2, "Second task", Status.TODO)
-
     items = [item1, item2]
 
+    # when
     indexViewModel = IndexViewModel(items)
 
+    # then
     assert indexViewModel.items == items
 
-#def test_to_do_items():
-#    item1 = ToDoItem(1, "First task", Status.TODO)
-#    item2 = ToDoItem(2, "Second task", Status.DOING)
-#
-#    items = [item1, item2]
-#
-#    to_do_items = [item1]
-#
-#    indexViewModel = IndexViewModel(items)
-#
-#    assert indexViewModel.to_do_items == to_do_items
+def test_to_do_items():
+    # given
+    item1 = ToDoItem(1, "First task", Status.TODO)
+    item2 = ToDoItem(2, "Second task", Status.DOING)
+    items = [item1, item2]
+    to_do_items = [item1]
+
+    # when
+    indexViewModel = IndexViewModel(items)
+
+    # then
+    assert indexViewModel.to_do_items == to_do_items
+
+def test_doing_items():
+    # given
+    item1 = ToDoItem(1, "First task", Status.TODO)
+    item2 = ToDoItem(2, "Second task", Status.DOING)
+    items = [item1, item2]
+    doing_items = [item2]
+
+    # when
+    indexViewModel = IndexViewModel(items)
+
+    # then
+    assert indexViewModel.doing_items == doing_items
+
+def test_done_items():
+    # given
+    item1 = ToDoItem(1, "First task", Status.TODO)
+    item2 = ToDoItem(2, "Second task", Status.DOING)
+    item3 = ToDoItem(3, "Third task", Status.DONE)
+    item4 = ToDoItem(4, "Fourth task", Status.DONE)
+
+    items = [item1, item2, item3, item4]
+    done_items = [item3, item4]
+
+    # when
+    indexViewModel = IndexViewModel(items)
+
+    # then
+    assert indexViewModel.done_items == done_items
--- viewModels/indexViewModel.py
+++ viewModels/indexViewModel.py
@@ -1,3 +1,5 @@
+from to_do_item import Status
+
 class IndexViewModel:
     def __init__(self, items):
         self._items = items
@@ -5,3 +7,15 @@ class IndexViewModel:
     @property
     def items(self):
         return self._items
+
+    @property
+    def to_do_items(self):
+        return list(filter(lambda item: item.status == Status.TODO, self._items))
+
+    @property
+    def doing_items(self):
+        return list(filter(lambda item: item.status == Status.DOING, self._items))
+
+    @property
+    def done_items(self):
+        return list(filter(lambda item: item.status == Status.DONE, self._items))
