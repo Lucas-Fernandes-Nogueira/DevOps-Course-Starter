95b26acbc5271e17170ebb5899355ab70b1cf432
Lucas Nogueira <lucas.nogueira@softiwre.com>
2021-06-23 15:35:09 +0100

    M11: remove heroku lines from deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,3 @@ docker build --target production --tag lucnog/todo-app:$TRAVIS_COMMIT .
 docker push lucnog/todo-app:$TRAVIS_COMMIT
 docker tag lucnog/todo-app:$TRAVIS_COMMIT lucnog/todo-app:latest
 docker push lucnog/todo-app:latest
-docker tag lucnog/todo-app:latest registry.heroku.com/devops-todo-app/web
-heroku container:login
-docker push registry.heroku.com/devops-todo-app/web
-heroku container:release web --app devops-todo-app
