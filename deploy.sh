19443e2318e07e66a489551e73bb7d31daa57cc8
Lucas Nogueira <lucas.nogueira@softiwre.com>
2021-06-23 17:48:36 +0100

    M11: add webhook to travis

--- deploy.sh
+++ deploy.sh
@@ -3,3 +3,4 @@ docker build --target production --tag lucnog/todo-app:$TRAVIS_COMMIT .
 docker push lucnog/todo-app:$TRAVIS_COMMIT
 docker tag lucnog/todo-app:$TRAVIS_COMMIT lucnog/todo-app:latest
 docker push lucnog/todo-app:latest
+curl -dH -X POST $WEBHOOK
